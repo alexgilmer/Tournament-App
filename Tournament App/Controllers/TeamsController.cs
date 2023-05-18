846e1a33482bb1529f6a514d785dcea8008fcfdd
Alex Gilmer <alex.gilmer@mitt.ca>
2023-05-18 12:28:23 -0500

    Add Teams Index page with leaderboard

---
+++ Tournament App/Controllers/TeamsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tournament_App.Data;
+using Tournament_App.Models;
+using Tournament_App.Models.ViewModels.Teams;
+
+namespace Tournament_App.Controllers
+{
+    public class TeamsController : Controller
+    {
+        private ApplicationDbContext Database { get; }
+
+        public TeamsController(ApplicationDbContext db)
+        {
+            Database = db;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            // AsEnumerable() forces the DB to resolve the results
+            // prevents an error of translating LINQ to SQL
+            var groups = Database.ApplicationUsers
+                .Include(u => u.Team)
+                .AsEnumerable()
+                .GroupBy(u => u.Team)
+                .Select(g => new IndexViewModel.Group
+                {
+                    Name = g.Key == null ? "Unassigned" : g.Key.Name,
+                    Points = g.Key == null ? 0 : g.Key.TeamAnswers.Sum(ta => ta.Answer.PointValue),
+                    Members = g.Select(m => new IndexViewModel.Member
+                    {
+                        Name = m.UserName,
+                        ApplicationUserId = m.Id
+                    }).ToList()
+                })
+                .OrderByDescending(g => g.Points)
+                .ToList();
+
+            // Insert data into view model
+            var vm = new IndexViewModel() { Groups = groups };
+
+            return View(vm);
+        }
+    }
+}
---
+++ Tournament App/Models/ViewModels/Teams/IndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Tournament_App.Models.ViewModels.Teams
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<Group> Groups { get; init; }
+
+        public class Group
+        {
+            public string Name { get; init; }
+            public int Points { get; init; }
+            public IList<Member> Members { get; init; }
+        }
+
+        public class Member
+        {
+            public string ApplicationUserId { get; init; }
+            public string Name { get; init; }
+        }
+    }
+}
