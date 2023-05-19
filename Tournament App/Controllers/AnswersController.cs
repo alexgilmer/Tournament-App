e4a56b557e5d33344e34d11b2182f985c7229529
Alex Gilmer <alex.gilmer@mitt.ca>
2023-05-19 14:41:38 -0500

    Set up Answer creation

---
+++ Tournament App/Controllers/AnswersController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Tournament_App.Data;
+using Tournament_App.Models;
+using Tournament_App.Models.ViewModels.Answers;
+
+namespace Tournament_App.Controllers
+{
+    [Authorize(Roles = Constants.AdminRole)]
+    public class AnswersController : Controller
+    {
+        private ApplicationDbContext Database { get; }
+
+        public AnswersController(ApplicationDbContext db)
+        {
+            Database = db;
+        }
+
+        public IActionResult Index()
+        {
+            var answers = Database.Answers.ToList();
+            return View(answers);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateAnswerFormModel vm)
+        {
+            Answer newAnswer = new()
+            {
+                Name = vm.Name,
+                Code = vm.Code,
+                Description = vm.Description,
+                PointValue = vm.PointValue
+            };
+            Database.Answers.Add(newAnswer);
+            Database.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Tournament App/Models/ViewModels/Answers/CreateAnswerFormModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Tournament_App.Models.ViewModels.Answers
+{
+    public class CreateAnswerFormModel
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public int PointValue { get; set; }
+    }
+}
