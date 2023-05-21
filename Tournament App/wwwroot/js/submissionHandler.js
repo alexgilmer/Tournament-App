fb93b6078733643d5bc0c51b49b4b480e0697796
Alex Gilmer <alex.gilmer@mitt.ca>
2023-05-21 18:56:59 -0500

    Add answer-submission toasts

--- Tournament App/wwwroot/js/submissionHandler.js
+++ Tournament App/wwwroot/js/submissionHandler.js
@@ -23,9 +23,52 @@ function handleSubmission(event) {
 }
 
 function successFunction(response) {
-    console.log(response);
+    const toastContainer = document.querySelector(".toast-container");
+    toastContainer.insertAdjacentHTML("beforeend", getToastHtml(response));
+    showLatestToast();
 }
 
 function errorFunction(response) {
     console.log("Error function engaged.", response);
+}
+
+function getToastHtml(data) {
+    console.log("This happened with data: ", data);
+
+    // success(green), warning(yellow), danger(red)
+    let bgColor = "success";
+    let textColor = "white";
+
+    if (!data.validCode) {
+        bgColor = "warning";
+        textColor = "dark"
+    }
+
+    if (data.pointsAwarded < 0) {
+        bgColor = "danger";
+        textColor = "white";
+    }
+
+    //data.ValidCode;
+    //data.AnswerName;
+    //data.PointsAwarded;
+    //data.Message;
+
+    return `
+    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="toast-header bg-${bgColor} text-${textColor}">
+          <strong class="me-auto">${data.answerName}</strong>
+          <small class="text-${textColor}">${data.pointsAwarded} points</small>
+          <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+        <div class="toast-body">
+          ${data.message}
+        </div>
+     </div>`;
+}
+
+function showLatestToast() {
+    const toastElem = document.querySelector(".toast-container .toast:last-of-type");
+    const toast = new bootstrap.Toast(toastElem);
+    toast.show();
 }
