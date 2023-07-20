17883be3f0311666f2eacbc7abb9b7afbe3244cf
Alex Gilmer <alex.gilmer@mitt.ca>
2023-07-20 15:57:52 -0500

    Set up create-notification-helper.js

--- Tournament App/wwwroot/js/create-notification-helper.js
+++ Tournament App/wwwroot/js/create-notification-helper.js
@@ -1,15 +1,25 @@
-$("#Title").on("change", (event) => {
-    console.log(event);
+$("#Title").on("input", (event) => {
+    let inputText = event.target.value;
+    if (!inputText)
+        inputText = "Title Text";
+
+    $(".notification-header").text(inputText);
 });
 
-$("#Text").on("change", (event) => {
-    console.log(event);
+$("#Text").on("input", (event) => {
+    let inputText = event.target.value;
+    if (!inputText)
+        inputText = "Body text";
+
+    $(".notification-content").text(inputText);
 });
 
 $("#HeaderColor").on("change", (event) => {
-    console.log(event);
+    $(".notification-header").css("background-color", event.target.value);
 });
 
-$("#TextColor").on("change", (event) => {
-    console.log(event);
-});
+$(".color-sample").on("click", (event) => {
+    const color = event.target.dataset.color;
+    $("#HeaderColor").val(color);
+    $("#HeaderColor").trigger("change");
+})
