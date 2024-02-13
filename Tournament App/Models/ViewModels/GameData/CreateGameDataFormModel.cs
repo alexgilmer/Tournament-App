843ec97e484bf8b5415cdafb7db3630321c2ee64
Alex Gilmer <alex.gilmer@mitt.ca>
2024-02-13 10:43:22 -0600

    Remove needless properties from CreateGameDataFormModel

--- Tournament App/Models/ViewModels/GameData/CreateGameDataFormModel.cs
+++ Tournament App/Models/ViewModels/GameData/CreateGameDataFormModel.cs
@@ -2,11 +2,9 @@
 {
     public class CreateGameDataFormModel
     {
-        public string Key { get; set; }
         public string? Description { get; set; }
         public string? FileName { get; set; }
         public IFormFile? File { get; set; }
-        public string Data { get; set; }
         public virtual ICollection<Guid> TeamIds { get; set; }
     }
 }
